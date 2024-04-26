a8093559c8662095c5ec4223c957d8207e8542a9
lowellteddy <lowellteddy88@gmail.com>
2024-04-26 20:39:41 +0300

    Added form

--- props/src/App.js
+++ props/src/App.js
@@ -1,14 +1,19 @@
+import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Transactions from './components/Transactions';
+import Form from './components/Form';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar/>
       
-      <Transactions />
-      
+      <Form/>
+      <Transactions/>
+     
+
     </div>
   );
 }
