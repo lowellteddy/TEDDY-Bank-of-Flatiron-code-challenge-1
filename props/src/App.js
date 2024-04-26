4bacc5e3c2d8bdd258491fbb5a6030d924622514
lowellteddy <lowellteddy88@gmail.com>
2024-04-26 18:53:44 +0300

    added columns

---
+++ props/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Transaction from './components/Transaction';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Transaction />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ props/src/components/Data.js
@@ -0,0 +1,35 @@
+export const bankTransactions = [
+    {
+        id: 1,
+        date: "24/04/2024",
+        description: "snacks",
+        category: "foods",
+        amount: 20,
+        quantity: 100
+    },
+    {
+        id: 2,
+        date: "21/06/2021",
+        description: "movies",
+        category: "entertainment",
+        amount: 20,
+        quantity: 150
+    },
+    {
+        id: 3,
+        date: "20/01/2020",
+        description: "snacks",
+        category: "foods",
+        amount: 20,
+        quantity: 117
+    },
+    {
+        id: 4,
+        date: "02/01/2019",
+        description: "videos",
+        category: "entertainment",
+        amount: 200,
+        quantity: 14
+    },
+    
+]
